a0c6531d3e3d791f94f0780913ffde50204c336a
rowanstoneking <97303860+rowanstoneking@users.noreply.github.com>
2025-10-01 13:31:08 -0400

    DeleteSubStrByIndex

--- CSharpToolbox/CSharpToolbox/StrFunc.cs
+++ CSharpToolbox/CSharpToolbox/StrFunc.cs
@@ -3,13 +3,31 @@
 public partial class ToolboxFunc
 {
     /// <summary>
+    /// Deletes subsection of string by indices.
+    /// </summary>
+    /// <param name="src"> Given string </param>
+    /// <param name="indexDeleteStart"> First index to delete </param>
+    /// <param name="indexDeleteEnd"> Last index to delete </param>
+    /// <returns> N/A </returns>
+    internal static void DeleteSubStrByIndex(ref string src, int indexDeleteStart, int indexDeleteEnd)
+    {
+        if ((src.Length < 1) || (indexDeleteStart >= indexDeleteEnd)) { return; }  // No deletions if original input if empty, or if there isn't space between start and end of delete section.
+        // ---
+        indexDeleteStart = (indexDeleteStart < 0) ? 0 : indexDeleteStart;  // Sets delete start to beginning if too small.
+        indexDeleteEnd = (indexDeleteEnd >= src.Length) ? src.Length - 1 : indexDeleteEnd;  // Sets delete end to end if too large.
+        // ---
+        src = src.Remove(indexDeleteStart, (indexDeleteEnd - indexDeleteStart + 1));  // Removes section
+    }
+
+
+    /// <summary>
     /// Gets portion of string from original
     /// </summary>
-    /// <param name="input">Given string</param>
-    /// <param name="indexSubMin">First index of subsection to be retrieved; assumed to be first of original if null</param>
-    /// <param name="indexSubMax">Last index of subsection to be retrieved; assumed to be last of original if null</param>
-    /// <param name="inclusive">Whether first and last indices are to be included in result</param>
-    /// <returns>String subsection (or empty string, if no material found)</returns>
+    /// <param name="input"> Given string </param>
+    /// <param name="indexSubMin"> First index of subsection to be retrieved; assumed to be first of original if null </param>
+    /// <param name="indexSubMax"> Last index of subsection to be retrieved; assumed to be last of original if null </param>
+    /// <param name="inclusive"> Whether first and last indices are to be included in result </param>
+    /// <returns> String subsection (or empty string, if no material found) </returns>
     internal static string GetStrSect(string input, int? indexSubMin = null, int? indexSubMax = null, bool inclusive = true)
     {
         const int indexMin = 0;  // min index of original string
